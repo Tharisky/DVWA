52969bbb4a6081f546e564487efb6ec83e079195
Tharisky <93770757+Tharisky@users.noreply.github.com>
2023-11-07 13:36:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 
 dvwaPageStartup( array( 'authenticated' ) );
 
-$page = dvwaPageNewGrab();
-$page[ 'title' ]   = 'Welcome' . $page[ 'title_separator' ].$page[ 'title' ];
-$page[ 'page_id' ] = 'home';
+page = dvwaPageNewGrab();
+page[ 'title' ]   = 'Welcome' . $page[ 'title_separator' ].$page[ 'title' ];
+page[ 'page_id' ] = 'home';
 
 $page[ 'body' ] .= "
 <div class=\"body_padded\">
