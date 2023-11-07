dd0abe3580111974725e98065d5d7469740f055c
Tharisky <93770757+Tharisky@users.noreply.github.com>
2023-11-07 13:27:31 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,11 @@
 <?php
 
-define 'DVWA_WEB_PAGE_TO_ROOT', '' );
+define ('DVWA_WEB_PAGE_TO_ROOT', '' );
 require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 
 dvwaPageStartup( array( ) );
 
-dvwaDatabaseConnect();
+dvwaDatabaseConnect()
 
 if( isset( $_POST[ 'Login' ] ) ) {
 	// Anti-CSRF
